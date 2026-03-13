fa5ac067198597f992a5f199a6f29b79306a2afc
Amanda Severo <miwafoxl@proton.me>
2026-03-13 09:57:18 -0300

    added initializer for element type Descriptor

--- src/Element/Descriptor.gd
+++ src/Element/Descriptor.gd
@@ -4,3 +4,8 @@ class_name Descriptor
 @export var alias: String = "" # Required: Lowercase, no symbols or spaces
 @export var short: String = ""; # Short phrase that defines the descriptor
 @export var long: String = ""; # Long explanation, BBCode or Markdown
+
+func _init(__alias: String, __short: String = "", __long: String = "") -> void:
+	self.alias = __alias
+	self.short = __short
+	self.long = __long
