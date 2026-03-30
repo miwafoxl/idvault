07b87830ac00dabcda52fc311d5d303c203aa46e
Amanda Severo <miwafoxl@proton.me>
2026-03-30 15:48:28 -0300

    actions: add action to append to items to selection by item ID

---
+++ actions/item/select/ByItemID_Append.gd
@@ -0,0 +1,12 @@
+extends Object
+
+## Append selection of items by Item ID. If items aren't existent, nothing happens.
+func run(__manager: Manager, __args: Array) -> bool: 
+	# Each int in the args array is an Item ID
+	var __item_ids: Array[int]
+	for __id: int in __args:
+		if __id is int:
+			__item_ids.append(__id)
+	var __get_id: Array[Item] = __manager.get_item_by_id(__item_ids)
+	__manager.select_items(__get_id, true)
+	return true
