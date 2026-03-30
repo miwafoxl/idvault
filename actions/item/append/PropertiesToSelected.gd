02731b83a0e0cebfe22ebabb3113b4b5497122c0
Amanda Severo <miwafoxl@proton.me>
2026-03-30 16:12:43 -0300

    actions: add action to add properties to selected items

---
+++ actions/item/append/PropertiesToSelected.gd
@@ -0,0 +1,17 @@
+extends Object
+
+## Append proprieties to selected items
+func run(__manager: Manager, __args: Array) -> bool:
+	# Every item in __args is a property
+	if __manager.selected_items.is_empty():
+		push_warning("items.append.property_to_selected: Nothing selected")
+		return false
+	var __properties: Array[Propriety] = []
+	for __prop: Variant in __args:
+		if __prop is Propriety:
+			__properties.append(__prop as Propriety)
+	var __success: bool = true
+	for __item: Item in __manager.selected_items: # TODO: Make this less destructive in case it fails
+		if not __item.append_proprieties(__properties):
+			__success = false
+	return __success
