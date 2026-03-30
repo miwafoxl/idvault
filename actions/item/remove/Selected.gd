5e005959ddd6afb5635e6edf5ba0836b8aee1c57
Amanda Severo <miwafoxl@proton.me>
2026-03-30 16:13:12 -0300

    actions: add action to remove selected items

---
+++ actions/item/remove/Selected.gd
@@ -0,0 +1,14 @@
+extends Object
+
+## Delete selected items. Run with caution.
+func run(__manager: Manager, __args: Array) -> bool:
+	if __manager.selected_items.is_empty():
+		push_warning("items.remove.selected: Nothing selected")
+		return false
+	var __rm_indexes: Array[int]
+	for __item: Item in __manager.selected_items:
+		var __idx: int = __manager.unordered_items.find(__item)
+		__rm_indexes.append(__idx)
+	__manager.selected_items.clear() # Deselects
+	__manager.remove_items(__rm_indexes)
+	return true
