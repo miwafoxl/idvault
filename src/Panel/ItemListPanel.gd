e1ca56a6ac861ab2f7332bf376803796504977bd
Amanda Severo <miwafoxl@proton.me>
2026-03-30 21:44:53 -0300

    ItemListPanel: support selecting and deselecting signals from ItemDisplayWidget

--- src/Panel/ItemListPanel.gd
+++ src/Panel/ItemListPanel.gd
@@ -4,13 +4,38 @@ class_name ItemListPanel
 @export var widget_item: PackedScene
 @export var item_list_parent: Control
 @export var items_ref: Array[Item] = []
+@export var selected_item_ids: Array[int] = []
 
-signal click(id: int)
-signal hover(id: int)
 signal add_item
 signal query(text: String)
+signal select_item(id: int)
+signal deselect_item(id: int)
+signal deselect_all()
+signal select_item_append(id: int)
 
-func update() -> void:
+func item_display_widget_click(__id: int) -> void:
+	if KeyboardModifiers.is_shift_modifier:
+		if __id in selected_item_ids:
+			deselect_item.emit(__id)
+		else:
+			select_item_append.emit(__id)
+	else:
+		select_item.emit(__id)
+
+func update(__selected_items_id: Array[int] = []) -> void:
+	update_item_display()
+	update_selected_items(__selected_items_id)
+
+func update_selected_items(__selected: Array[int] = []) -> void:
+	selected_item_ids = __selected
+	for __item_widget: ItemDisplayWidget in item_list_parent.get_children(false):
+		if __item_widget.related_id in __selected:
+			__item_widget.display_selected = true
+		else:
+			__item_widget.display_selected = false
+		__item_widget.update_color()
+
+func update_item_display() -> void:
 	for __item: ItemDisplayWidget in item_list_parent.get_children(false):
 		__item.queue_free()
 	for __item: Item in items_ref:
@@ -22,8 +47,8 @@ func update() -> void:
 			__item_widget.subtitle = __item_title[0].alt
 		else:
 			__item_widget.title = str(__item.id)
-		__item_widget.click.connect(click.emit.bind(__item.id))
-		__item_widget.hover.connect(hover.emit.bind(__item.id))
+		__item_widget.click.connect(item_display_widget_click)
+		#__item_widget.hover.connect(hover.emit)
 		__item_widget.update()
 		item_list_parent.add_child(__item_widget)
 
--- src/UI.gd
+++ src/UI.gd
@@ -6,27 +6,45 @@ class_name UI
 @export var node_panels: Control
 
 func add_item() -> void:
-	action_manager.run(&"item.append.testitem")
+	action_manager.run(&"items.append.testitem")
 	update_panels.call_deferred()
 
 func select_item(__id: int) -> void:
-	action_manager.run(&"item.select.id")
+	action_manager.run(&"items.select.by_item_id", __id)
+	update_panels.call_deferred()
+
+func select_item_append(__id: int) -> void:
+	action_manager.run(&"items.select.by_item_id_append", __id)
+	update_panels.call_deferred()
+
+func deselect_item(__id: int) -> void:
+	action_manager.run(&"items.deselect.by_item_id", __id)
+	update_panels.call_deferred()
+
+func deselect_all() -> void:
+	action_manager.run(&"items.deselect.all")
+	update_panels.call_deferred()
 
 func query(__text: String) -> void:
 	action_manager.run(&"items.query.text")
+	update_panels.call_deferred()
 
 func update_panels() -> void:
 	for panel: Control in node_panels.get_children():
 		if (panel is UI_Panel) and (panel is ItemListPanel):
 			(panel as ItemListPanel).items_ref = manager.unordered_items
-			(panel as ItemListPanel).update()
+			(panel as ItemListPanel).update(manager.retrieve_selected_items_id())
 		
 func update_signals() -> void:
 	for panel: Control in node_panels.get_children():
-		if panel is UI_Panel:
-			if panel is ItemListPanel:
-				(panel as ItemListPanel).add_item.connect(add_item)
-				(panel as ItemListPanel).query.connect(query)
+		if panel is UI_Panel and (panel is ItemListPanel):
+			var __item_list: ItemListPanel = panel
+			__item_list.add_item.connect(add_item)
+			__item_list.select_item.connect(select_item)
+			__item_list.select_item_append.connect(select_item_append)
+			__item_list.deselect_item.connect(deselect_item)
+			__item_list.deselect_all.connect(deselect_all)
+			__item_list.query.connect(query)
 
 func _ready() -> void:
 	update_signals()
