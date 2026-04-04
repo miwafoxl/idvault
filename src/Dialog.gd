93a3bbb0d3a84a5daac738bca80d35b8a3eabc62
Amanda Severo <miwafoxl@proton.me>
2026-04-04 10:52:02 -0300

    dialog: add class for dialog boxes

---
+++ src/Dialog.gd
@@ -0,0 +1,16 @@
+extends Window
+class_name Dialog
+
+@export var args: Array = []
+@export var returns: bool = false
+var return_id: String = ""
+
+signal returning(__id: String, return_args: Array)
+
+func pop() -> void:
+	self.visible = true
+	self.popup_centered()
+
+func _ready() -> void:
+	self.visible = false
+	self.set_force_native(true)
