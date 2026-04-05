07faa2061387d6410f00e20e3116e52d7d05edd1
Amanda Severo <miwafoxl@proton.me>
2026-04-05 13:56:01 -0300

    action: update items.append.testitem run argument to support typed dictionary

--- actions/item/append/TestItem.gd
+++ actions/item/append/TestItem.gd
@@ -1,7 +1,7 @@
 extends Object
 
 ## Creates and appends an empty item
-func run(__manager: Manager, __args: Array[Variant]) -> bool:
+func run(__manager: Manager, __param: Dictionary) -> bool:
 	return __manager.append_items([Item.new(__manager.get_stage_size(), [
 		Display.new(RandomString.new("", 12).value, "first test here"\
 		.repeat(randi() % 2))
