bcef33f0828f9088c7acedc9566e059dec00d1b2
Amanda Severo <miwafoxl@proton.me>
2026-04-05 17:37:56 -0300

    menus: add Menu and MenuManager classes

---
+++ src/Menu.gd
@@ -0,0 +1,26 @@
+extends Resource
+class_name Menu
+
+@export var alias: StringName = &"";
+@export var src: GDScript = null
+var popup: PopupMenu
+
+func check() -> bool:
+	if src == null:
+		printerr("Menu: menu '%s' has no source. Skipping it.", alias)
+		return false
+	var __obj: Object = Object.new()
+	__obj.set_script(src)
+	if not __obj.has_method(&"build"):
+		printerr("Menu: source for menu '%s' has no build() method. Skipping it." % alias)
+		return false
+	__obj.free()
+	return true
+
+func get_menu() -> PopupMenu:
+	var __obj: Object = Object.new()
+	var __result: PopupMenu = null
+	__obj.set_script(src)
+	__result = __obj.build()
+	__obj.free()
+	return __result
---
+++ src/MenuManager.gd
@@ -0,0 +1,52 @@
+extends Node
+class_name MenuManager
+
+@export var default: Array[Menu] = [];
+@export var loaded: Dictionary[StringName, Variant] = {}; #  {alias: menu ref}
+@export var theme: Theme
+
+
+func append_menus(__menus: Array[Menu], __log: bool = false) -> void:
+	if __menus.is_empty(): return
+	var __loaded_menus: PackedStringArray
+	for i: int in __menus.size():
+		var __cur: Menu = __menus[i]
+		var __ref: WeakRef = null
+		if __cur.alias.is_empty():
+			printerr("MenuManager: menu id on index %s has no alias. Skipping it." % i)
+			continue
+		if __cur.alias in __loaded_menus:
+			printerr("MenuManager: menu id '%s' has the same alias as a previously loaded menu. Please unload that one then append it." % i)
+			continue
+		__ref = weakref(__cur)
+		loaded.set(__cur.alias, __ref)
+		__loaded_menus.append(__cur.alias)
+	loaded.sort()
+	if __log:
+		__loaded_menus.sort()
+		print("MenuManager: %s loaded:\n- %s" % [__loaded_menus.size(), \
+			"\n- ".join(__loaded_menus)
+		])
+
+func remove_menu(__rm_menu_aliases: Array[StringName]) -> void:
+	if __rm_menu_aliases.is_empty(): return
+	for __alias: StringName in loaded.keys():
+		if __alias in __rm_menu_aliases:
+			loaded.erase(__alias)
+
+func retrieve_menu(__menu_id: StringName, __param_dict: Dictionary = {}) -> PopupMenu:
+	# TODO: Support menu parameters
+	var __ref: WeakRef = loaded.get(__menu_id, null)
+	var __menu: Menu = null
+	if __ref == null:
+		printerr("MenuManager: menu id '%s' not found or loaded." % __menu_id)
+		return null
+	__menu = __ref.get_ref()
+	if __menu == null:
+		printerr("MenuManager: failed to get a reference to menu id '%s'." % __menu_id)
+		return null
+	var __result: PopupMenu = __menu.get_menu()
+	__result.set_theme(theme)
+	if __result == null:
+		push_warning("MenuManager: menu id '%s' failed." % __menu_id)
+	return __result
