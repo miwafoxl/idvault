dc7af50d5824b743a02705ff7a71bb70e2787d46
Amanda Severo <miwafoxl@proton.me>
2026-04-05 14:00:14 -0300

    action: update items.remove.property_id parameter processing to support typed dictionary

--- actions/item/remove/PropertyID.gd
+++ actions/item/remove/PropertyID.gd
@@ -1,24 +1,28 @@
 extends Object
 
 ## Removes specific properties from items by property ID
-func run(__manager: Manager, __args: Array) -> bool:
+func run(__manager: Manager, __param: Dictionary) -> bool:
 	var __items: Array[Item] = []
 	var __rm_property_ids: Array[String] = []
-	for i: int in __args.size():
-		match i:
-			0: # Array of Items
-				assert(__args[i] is Array)
-				for __arg in __args[i]:
+	#region Parameter processing
+	for __key: String in __param:
+		var __value: Variant = __param[__key]
+		match __key:
+			"item" when __value is Array:
+				for __arg in __value:
 					if __arg is Item: __items.append(__arg as Item)
 				if __items.is_empty(): 
 					return false
-			1: # Array of strings (property IDs to remove)
-				assert(__args[i] is Array)
-				for __arg in __args[i]:
+			"property_id" when __value is Array:
+				for __arg in __value:
 					if __arg is String: 
 						__rm_property_ids.append(__arg as String)
 				if __rm_property_ids.is_empty():
 					return false
+			_:
+				push_warning("item.remove.property_id: invalid key '%s'\
+				-> item, property_id" % __key)
+	#endregion Parameter processing
 	for __item: Item in __items:
 		__item.remove_properties(__rm_property_ids)
 	return true
