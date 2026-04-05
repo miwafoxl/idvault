a591f140a3210418157c3f5407c86f709c568257
Amanda Severo <miwafoxl@proton.me>
2026-04-05 13:59:14 -0300

    action: update items.deselect.all parameter processing to support typed dictionary

--- actions/item/remove/ByItemID.gd
+++ actions/item/remove/ByItemID.gd
@@ -1,13 +1,21 @@
 extends Object
 
 ## Removes items by ID
-func run(__manager: Manager, __args: Array) -> bool: 
-	# Each int in the args array is an Item ID
+func run(__manager: Manager, __param: Dictionary) -> bool: 
 	var __item_ids: Array[int]
 	var __rm_indexes: Array[int]
-	for __id: int in __args:
-		if __id is int:
-			__item_ids.append(__id)
+	#region Parameter processing
+	for __key: String in __param:
+		var __value: Variant = __param[__key]
+		match __key:
+			"item_id" when __value is Array:
+				for __id: int in __value:
+					if __id is int:
+						__item_ids.append(__id)
+			_:
+				push_warning("items.remove.by_item_id: invalid key '%s'\
+				-> item_id" % __key)
+	#endregion Parameter processing
 	var __get_id: Array[Item] = __manager.get_item_by_id(__item_ids)
 	for __item: Item in __get_id:
 		var __idx: int = __manager.unordered_items.find(__item)
