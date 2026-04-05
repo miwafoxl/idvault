00f6221a3b21dc806693a672741a39bf27693c8c
Amanda Severo <miwafoxl@proton.me>
2026-04-05 12:36:21 -0300

    ItemDisplayWidget: remove mouse_on_top flag

--- src/Widget/ItemDisplayWidget.gd
+++ src/Widget/ItemDisplayWidget.gd
@@ -5,7 +5,6 @@ class_name ItemDisplayWidget
 @export var title: String = "";
 @export var subtitle: String = "";
 var display_selected: bool = false
-var mouse_on_top: bool = false
 
 @export var node_title: Label 
 @export var node_subtitle: Label
@@ -19,28 +18,12 @@ func update() -> void:
 	update_color()
 
 func update_color() -> void:
-	if mouse_on_top:
-		if display_selected:
-			self.set_modulate(Color(0.0, 1.0, 0.0, 1.0))
-		else:
-			self.set_modulate(Color(1.0, 0.0, 1.0, 1.0))
-	if not mouse_on_top:
-		if display_selected:
-			self.set_modulate(Color(0.0, 0.0, 1.0, 1.0))
-		else:
-			self.set_modulate(Color.WHITE)
+	if display_selected:
+		self.set_modulate(Color(0.537, 0.537, 1.0, 1.0))
+	else:
+		self.set_modulate(Color.WHITE)
 
 #region Theming
-
-func _on_mouse_entered() -> void:
-	if not mouse_on_top:
-		mouse_on_top = true
-		update_color()
-
-func _on_mouse_exited() -> void:
-	if mouse_on_top:
-		mouse_on_top = false
-		update_color()
 
 func _on_gui_input(event: InputEvent) -> void:
 	if event is not InputEventMouseButton: return
