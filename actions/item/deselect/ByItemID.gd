1850b0cdff6f7a4dbde8d09aab781395c8b19da9
Amanda Severo <miwafoxl@proton.me>
2026-04-05 13:57:31 -0300

    action: update items.deselect.by_item_id parameter processing to support typed dictionary

--- actions/item/deselect/ByItemID.gd
+++ actions/item/deselect/ByItemID.gd
@@ -1,12 +1,21 @@
 extends Object
 
 ## Deselect items. If items aren't selected, nothing happens.
-func run(__manager: Manager, __args: Array) -> bool: 
+func run(__manager: Manager, __param: Dictionary) -> bool: 
 	# Each int in the args array is an Item ID
 	var __item_ids: Array[int]
-	for __id: int in __args:
-		if __id is int:
-			__item_ids.append(__id)
+	#region Parameter processing
+	for __key: String in __param:
+		var __value: Variant = __param[__key]
+		match __key:
+			"item_id" when __value is Array:
+				for __id: int in __value:
+					if __id is int:
+						__item_ids.append(__id)
+			_:
+				push_warning("items.deselect.by_item_id: invalid key '%s'\
+				-> item_id" % __key)
+	#endregion Parameter processing
 	var __get_id: Array[Item] = __manager.get_item_by_id(__item_ids)
 	__manager.deselect_items(__get_id)
 	return true
