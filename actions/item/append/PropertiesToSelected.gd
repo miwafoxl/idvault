9a0d1f8692e8d0b67f167d9cdb56c5e621df7af0
Amanda Severo <miwafoxl@proton.me>
2026-04-05 13:55:21 -0300

    action: update items.append.properties_to_selected parameter processing to support typed dictionary

--- actions/item/append/PropertiesToSelected.gd
+++ actions/item/append/PropertiesToSelected.gd
@@ -1,15 +1,26 @@
 extends Object
 
 ## Append properties to selected items
-func run(__manager: Manager, __args: Array) -> bool:
+func run(__manager: Manager, __param: Dictionary) -> bool:
 	# Every item in __args is a property
+	var __properties: Array[Property] = []
 	if __manager.selected_items.is_empty():
 		push_warning("items.append.property_to_selected: Nothing selected")
 		return false
-	var __properties: Array[Property] = []
-	for __prop: Variant in __args:
-		if __prop is Property:
-			__properties.append(__prop as Property)
+	#region Parameter processing
+	for __key: String in __param:
+		var __value: Variant = __param[__key]
+		match __key:
+			"properties" when __value is Array:
+				for __property: Variant in __value:
+					if __property is Property:
+						__properties.append(__property as Property)
+				if __properties.is_empty(): 
+					return false
+			_:
+				push_warning("items.append.property_to_selected: invalid key '%s'\
+				-> properties" % __key)
+	#endregion Parameter processing
 	var __success: bool = true
 	for __item: Item in __manager.selected_items: # TODO: Make this less destructive in case it fails
 		if not __item.append_properties(__properties):
