c28cff7a21fb699fd644d923a8ee95704a1edaa1
Amanda Severo <miwafoxl@proton.me>
2026-04-05 14:01:17 -0300

    action: update items.select.by_item_id_append parameter processing to support typed dictionary

--- actions/item/select/ByItemID_Append.gd
+++ actions/item/select/ByItemID_Append.gd
@@ -1,12 +1,21 @@
 extends Object
 
 ## Append selection of items by Item ID. If items aren't existent, nothing happens.
-func run(__manager: Manager, __args: Array) -> bool: 
+func run(__manager: Manager, __param: Dictionary) -> bool: 
 	# Each int in the args array is an Item ID
 	var __item_ids: Array[int]
-	for __id: int in __args:
-		if __id is int:
-			__item_ids.append(__id)
+	#region Parameter processing
+	for __key: String in __param:
+		var __value: Variant = __param[__key]
+		match __key:
+			"item_id" when __value is Array:
+				for __id: int in __value:
+					if __id is int:
+						__item_ids.append(__id)
+			_:
+				push_warning("items.select.by_item_id_append: invalid key '%s'\
+				-> item_id" % __key)
+	#endregion Parameter processing
 	var __get_id: Array[Item] = __manager.get_item_by_id(__item_ids)
 	__manager.select_items(__get_id, true)
 	return true
