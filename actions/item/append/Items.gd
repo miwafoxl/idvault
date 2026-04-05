66e75d212de0b6127afbca2b4f41d6fffdb1ab5b
Amanda Severo <miwafoxl@proton.me>
2026-04-05 13:54:51 -0300

    action: update items.append.items parameter processing to support typed dictionary

--- actions/item/append/Items.gd
+++ actions/item/append/Items.gd
@@ -1,26 +1,27 @@
 extends Object
 
 ## Creates and appends a specified amount of empty items
-func run(__manager: Manager, __args: Array) -> bool: 
-	# [0 = starting id, 1 = count, 2 = properties]
+func run(__manager: Manager, __param: Dictionary) -> bool: 
 	var __id: int = __manager.get_stage_size()
 	var __range: int = 1
 	var __init_props: Array[Property] = []
-	for i: int in __args.size():
-		match i:
-			0: 
-				assert(__args[i] is int)
-				if __args[i] <= __id or __args[i] == 0: continue
-				__id = __args[i]
-			1: 
-				assert(__args[i] is int)
-				__range = max(1, __args[i] % 1000)
-			2:
-				assert(__args[i] is Array)
-				for __arg in __args[i]:
-					if __arg is Property: __init_props.append(__arg as Property)
-				if __init_props.is_empty(): 
-					return false
+	#region Parameter processing
+	for __key: String in __param:
+		var __value: Variant = __param[__key]
+		match __key:
+			"item_id" when __value is int:
+				if __value <= __id or __value == 0: continue
+				__id = __value
+			"count" when __value is int:
+				__range = max(1, __value % 1000)
+			"properties" when __value is Array:
+				for __property: Variant in __value:
+					if __property is Property:
+						__init_props.append(__property as Property)
+			_:
+				push_warning("item.append.items: invalid key '%s'\
+				-> item_id, count, properties" % __key)
+	#endregion Parameter processing
 	var __ids: Array = range(__id, __id + __range)
 	var __items: Array[Item] = []	
 	for i: int in __ids:
