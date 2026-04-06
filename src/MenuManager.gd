f326159e495b835b6ae3229601c6f3dd35f36ee5
Amanda Severo <miwafoxl@proton.me>
2026-04-05 23:02:31 -0300

    Menu: call action when item in menu is pressed

--- src/MenuManager.gd
+++ src/MenuManager.gd
@@ -4,6 +4,7 @@ class_name MenuManager
 @export var default: Array[Menu] = [];
 @export var loaded: Dictionary[StringName, Variant] = {}; #  {alias: menu ref}
 @export var theme: Theme
+@export var action_manager: ActionManager
 
 
 func append_menus(__menus: Array[Menu], __log: bool = false) -> void:
@@ -54,4 +55,4 @@ func retrieve_menu(__menu_id: StringName, __param_dict: Dictionary = {}) -> Cont
 func menu_action_callback(__param: Dictionary) -> void:
 	if __param.is_empty(): return
 	var __action: StringName = __param.keys()[0]
-	print_debug(__param)
+	action_manager.run(__action, __param.values()[0])
