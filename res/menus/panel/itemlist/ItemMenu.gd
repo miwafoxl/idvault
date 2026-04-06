5a28f50a09bc75e4e0a8a166878687f0064673d7
Amanda Severo <miwafoxl@proton.me>
2026-04-06 02:13:18 -0300

    Menu: update panel.itemlist.item_menu to use item String id

--- res/menus/panel/itemlist/ItemMenu.gd
+++ res/menus/panel/itemlist/ItemMenu.gd
@@ -1,17 +1,17 @@
 extends Object
 
-## Describe your menu here
+## Menu containing main item options
 func build(__param: Dictionary) -> ContextMenu:
 	var __menu: ContextMenu = ContextMenu.new()
-	var __items_id: Array[int] = []
+	var __items_id: Array[String] = []
 	var __items_size: int
 	#region Parameter processing
 	for __key: String in __param:
 		var __value: Variant = __param[__key]
 		match __key:
 			"item_id" when __value is Array:
-				for __id: int in __value:
-					if __id is int:
+				for __id: Variant in __value:
+					if __id is String:
 						__items_id.append(__id)
 			_:
 				push_warning("panel.itemlist.item_Menu: invalid key '%s'\
