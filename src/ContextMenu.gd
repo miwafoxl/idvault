4f0cbb2a70d2f04a136215914a1f3b0ea94273e9
Amanda Severo <miwafoxl@proton.me>
2026-04-06 02:14:53 -0300

    ContextMenu: fix menu not being freed after going invisible

--- src/ContextMenu.gd
+++ src/ContextMenu.gd
@@ -7,5 +7,9 @@ func item_pressed(__id: int) -> void:
 	var __param: Dictionary = self.get_item_metadata(__id)
 	action_query.emit(__param)
 
+func check_visibility() -> void:
+	if not visible: queue_free()
+
 func _ready() -> void:
 	id_pressed.connect(item_pressed)
+	visibility_changed.connect(check_visibility)
