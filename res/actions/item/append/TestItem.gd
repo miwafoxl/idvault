67b997d4a1147d4f2fe2e55d11a638ca40483782
Amanda Severo <miwafoxl@proton.me>
2026-04-08 09:30:45 -0300

    Actions: update items.append.testitem

--- res/actions/item/append/TestItem.gd
+++ res/actions/item/append/TestItem.gd
@@ -3,6 +3,6 @@ extends Object
 ## Creates and appends an empty item
 func run(__manager: ItemManager, __param: Dictionary) -> bool:
 	return __manager.append_items([Item.new("", [
-		Display.new(RandomString.new("", 12).value, "first test here"\
-		.repeat(randi() % 2))
+		Display.new(RandomString.new("", 12).value, "first test here".repeat(randi() % 2)),
+		Descriptor.new("hahaha")
 	])])
