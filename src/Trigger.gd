76a4817b0ce4b44b9bbbd150a14f5c21e5cfb3ed
Amanda Severo <miwafoxl@proton.me>
2026-04-08 11:03:40 -0300

    Trigger: add trigger class for triggering actions and dialogs

---
+++ src/Trigger.gd
@@ -0,0 +1,19 @@
+extends Resource
+class_name Trigger
+
+@export var type: TriggerTypes = TriggerTypes.TRIGGER
+@export var relevant_id: StringName = ""
+@export var parameters: Dictionary = {}
+
+func _init(__type: TriggerTypes, __relevant_id: StringName = &"", \
+		__param: Dictionary = {}) -> void:
+	self.type = __type
+	self.relevant_id = __relevant_id
+	self.parameters = __param
+
+enum TriggerTypes {
+	TRIGGER,
+	NONE,
+	ACTION,
+	DIALOG
+}
