514d5d849f615c8f9526e201ec645782961d9966
Amanda Severo <miwafoxl@proton.me>
2026-04-08 11:06:14 -0300

    Actions: update items.dialog.selected_item_properties to use Trigger

--- res/actions/item/dialog/SelectedItemProperties.gd
+++ res/actions/item/dialog/SelectedItemProperties.gd
@@ -2,6 +2,9 @@ extends Object
 
 ## Opens the item properties dialog
 func run(__manager: ItemManager, __param: Dictionary) -> bool:
-	__manager.trigger_dialog.emit(&"item_properties", {
-		"item": __manager.selected_items[0]}) # TODO: Support bulk edit
+	__manager.trigger.emit(Trigger.new(
+		Trigger.TriggerTypes.DIALOG,
+		&"item_properties", {
+			"item": __manager.selected_items[0]} 
+	)) # TODO: Support bulk edit
 	return true
