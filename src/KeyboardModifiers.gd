f9d84145d9e44e61f213918689c44600af52df6d
Amanda Severo <miwafoxl@proton.me>
2026-04-09 20:43:06 -0300

    KeyboardModifiers: use Godot's built-in actions for modifier detection

--- src/KeyboardModifiers.gd
+++ src/KeyboardModifiers.gd
@@ -6,11 +6,5 @@ extends Node
 func _input(event: InputEvent) -> void:
 	if event is not InputEventKey: return
 	print_debug(event as InputEventKey)
-	if (event as InputEventKey).keycode == 4194325 and event.is_pressed():
-		is_shift_modifier = true
-	if (event as InputEventKey).keycode == 4194325 and not event.is_pressed():
-		is_shift_modifier = false
-	if (event as InputEventKey).keycode == 4194328 and event.is_pressed():
-		is_alt_modifier = true
-	if (event as InputEventKey).keycode == 4194328 and not event.is_pressed():
-		is_alt_modifier = false
+	is_shift_modifier = event.is_action_pressed("shift_mod")
+	is_alt_modifier = event.is_action_pressed("alt_mod")
