e2e7f6d00b3739c5bb126da74a388b4a37414401
Amanda Severo <miwafoxl@proton.me>
2026-04-09 20:41:00 -0300

    Dialog: rename MessageBox to just Message; remove test_return dialog

--- src/Dialog/TestReturnDialog.gd
+++
@@ -1,8 +0,0 @@
-extends Dialog
-class_name TestReturnDialog
-
-@export var test_lineedit: LineEdit
-
-func _on_button_ok_button_up() -> void:
-	returning.emit(self.return_id, [test_lineedit.get_text()])
-	self.queue_free()
