0ca99d4f5c5215791e22dcc88a3ac31d394d5127
Amanda Severo <miwafoxl@proton.me>
2026-04-09 21:23:04 -0300

    PropertyEdit: add abstract method check_if_changed

--- src/Widget/PropertyWidget/DescriptorEdit.gd
+++ src/Widget/PropertyWidget/DescriptorEdit.gd
@@ -10,7 +10,16 @@ func deserialize(__property: Property) -> void:
 	related_prop_id = __prop.id
 	lineedit_alias.text = __prop.alias
 	lineedit_short.text = __prop.short
-	lineedit_short.text = __prop.long
+	lineedit_long.text = __prop.long
+	for __control: Control in [lineedit_alias, lineedit_short, lineedit_long]:
+		__control.set_meta(&"unchanged", __control.text)
+
+func check_if_changed() -> bool:
+	var __changed: bool = false
+	for __control: Control in [lineedit_alias, lineedit_short, lineedit_long]:
+		if not __control.get_meta(&"unchanged") == __control.text:
+			__changed = true
+	return __changed
 
 func get_as_property() -> Property:
 	var __alias: String = lineedit_alias.text.to_snake_case()
--- src/Widget/PropertyWidget/DisplayEdit.gd
+++ src/Widget/PropertyWidget/DisplayEdit.gd
@@ -9,6 +9,15 @@ func deserialize(__property: Property) -> void:
 	related_prop_id = __prop.id
 	lineedit_title.text = __prop.text
 	lineedit_alt.text = __prop.alt
+	for __lineedit: LineEdit in [lineedit_title, lineedit_alt]:
+		__lineedit.set_meta(&"unchanged", __lineedit.text)
+
+func check_if_changed() -> bool:
+	var __changed: bool = false
+	for __lineedit: LineEdit in [lineedit_title, lineedit_alt]:
+		if not __lineedit.get_meta(&"unchanged") == __lineedit.text:
+			__changed = true
+	return __changed
 
 func get_as_property() -> Property:
 	var __title: String = lineedit_title.text.strip_edges().strip_escapes()
