65d4ddda76c573ac531a15c2fa4432808b6d387a
Amanda Severo <miwafoxl@proton.me>
2026-04-10 11:07:18 -0300

    Items: support selection only if nothing selected for item select actions

--- res/actions/item/select/ByItemIndex.gd
+++ res/actions/item/select/ByItemIndex.gd
@@ -2,8 +2,8 @@ extends Object
 
 ## Select items by Item index. If items aren't existent, nothing happens.
 func run(__manager: ItemManager, __param: Dictionary) -> bool: 
-	# Each int in the args array is an Item ID
 	var __select_indexes: Array[int]
+	var __nothing_selected: bool = false
 	#region Parameter processing
 	for __key: String in __param:
 		var __value: Variant = __param[__key]
@@ -12,9 +12,14 @@ func run(__manager: ItemManager, __param: Dictionary) -> bool:
 				for __id: int in __value:
 					if __id is int:
 						__select_indexes.append(__id)
+			"only_if_nothing_selected" when __value is bool:
+				if __value is bool:
+					__nothing_selected = __value
 			_:
 				push_warning("items.select.by_item_index: invalid key '%s'\
-				-> item_idx" % __key)
+				-> item_idx, only_if_nothing_selected" % __key)
 	#endregion Parameter processing
+	if __nothing_selected:
+		if __manager.selected_items.is_empty(): return true
 	__manager.select_items_at_stage_index(__select_indexes)
 	return true
