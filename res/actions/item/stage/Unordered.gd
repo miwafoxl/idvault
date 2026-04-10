dbf8c7c263384eb2dc886c1ab6dad3f142c644f1
Amanda Severo <miwafoxl@proton.me>
2026-04-10 11:08:31 -0300

    Items: items.stage.* to use ItemManager unordered items if no items is provided to stage

--- res/actions/item/stage/Unordered.gd
+++ res/actions/item/stage/Unordered.gd
@@ -10,14 +10,8 @@ func run(__manager: ItemManager, __param: Dictionary) -> bool:
 			"item" when __value is Array:
 				for __arg in __value:
 					if __arg is Item: __items.append(__arg as Item)
-				if __items.is_empty(): 
-					return false
-			_:
-				push_warning("item.stage.alphabetical: invalid key '%s'\
-				-> item" % __key)
 	#endregion Parameter processing
 	if __items.is_empty():
-		push_warning("items.stage.unordered: No items to stage")
-		return false
+		__items = __manager.unordered_items
 	__manager.stage_items(__items)
 	return true
