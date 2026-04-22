21be18d1e5127e85f200d75db368c99b02c2011c
Amanda Severo <miwafoxl@proton.me>
2026-04-22 16:06:01 -0300

    Link: remove deprecated append/remove to id code

--- src/Property/Link.gd
+++ src/Property/Link.gd
@@ -5,30 +5,6 @@ class_name Link
 @export var to_id: String;
 @export var parameters: Dictionary[String, Variant] = {};
 
-# TODO: remove possibility of adding multiple of the same link
-#func append_linking_from_id(__linking_id: Array[int]) -> bool:
-	#if not __linking_id.is_empty():
-		#from.append_array(__linking_id)
-		#return true
-	#return false
-#
-#func append_linked_to_id(__linked_to: Array[int]) -> bool:
-	#if not __linked_to.is_empty():
-		#to.append_array(__linked_to)
-		#return true
-	#return false
-#
-#func remove_linked_to_id(__rm_indexes: Array[int]) -> bool:
-	#var __links: int = to.size()
-	#if not __rm_indexes.is_empty():
-		#for __linked_item_id: int in __rm_indexes:
-			#if __linked_item_id > __links: 
-				#printerr("Failed to remove property %s (larger than \
-				#properties size (%s))" % [__linked_item_id, __links])
-				#return false
-			#to.remove_at(__linked_item_id)
-	#return true
-
 func append_parameter(__parameters: Dictionary[String, Variant]) -> bool:
 	if not __parameters.is_empty():
 		parameters.merge(__parameters)
