145c99455cf80cad1fb3a1181b7cfef9a86c78a8
Amanda Severo <miwafoxl@proton.me>
2026-04-22 16:02:35 -0300

    DefaultUI: added LinkEdit property edit widget

---
+++ src/UI/DefaultUI/Widget/PropertyWidget/LinkEdit.gd
@@ -0,0 +1,43 @@
+extends PropertyWidget
+class_name LinkEditWidget
+
+@export var lineedit_title: LineEdit
+@export var lineedit_alt: LineEdit
+
+func deserialize(__property: Property) -> void:
+	var __prop: Display = __property
+	related_prop_id = __prop.id
+	lineedit_title.text = __prop.text
+	lineedit_alt.text = __prop.alt
+	for __lineedit: LineEdit in [lineedit_title, lineedit_alt]:
+		__lineedit.set_meta(&"unchanged", __lineedit.text)
+
+func check_if_changed() -> bool:
+	var __changed: bool = false
+	for __lineedit: LineEdit in [lineedit_title, lineedit_alt]:
+		if not __lineedit.get_meta(&"unchanged") == __lineedit.text:
+			__changed = true
+	return __changed
+
+func get_as_property() -> Property:
+	var __title: String = lineedit_title.text.strip_edges().strip_escapes()
+	var __alt: String = lineedit_alt.text.strip_edges().strip_escapes()
+	return Display.new(__title, __alt)
+
+func collect() -> Dictionary:
+	var __collected: Dictionary = {}
+	if marked_for_deletion:
+		__collected = {"rem": {
+			related_prop_id: marked_for_deletion
+		}}
+	elif check_if_changed():
+		__collected = {"mod": {
+			related_prop_id: get_as_property().deserialized()
+		}}
+	return __collected
+
+func trigger_options() -> void:
+	trigger.emit(Trigger.new(
+		Trigger.TriggerTypes.MENU,
+		&"dialog.item_properties.property_menu"
+	))
