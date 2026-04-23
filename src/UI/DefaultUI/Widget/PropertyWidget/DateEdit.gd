8f9377c4c0570c41a577b3e23359c8c6d8aa6644
Amanda Severo <miwafoxl@proton.me>
2026-04-23 00:14:39 -0300

    DateEdit: lambda for converting date UI values to Vector3i

--- src/UI/DefaultUI/Widget/PropertyWidget/DateEdit.gd
+++ src/UI/DefaultUI/Widget/PropertyWidget/DateEdit.gd
@@ -29,15 +29,20 @@ func check_if_changed() -> bool:
 # TODO: Make configurable when the user puts a number higher than the month currently
 # set, either modulate to current month (%) or max out 
 func get_as_property() -> Property:
+	var __get_yyyymmdd: Callable = func(__day: int, __year: int, __month_selected: int) -> Vector3i:
+		var __yyyymmdd: Vector3i = Vector3i.ZERO
+		__yyyymmdd.x = __year
+		__yyyymmdd.z = ((__day - 1) % DAYS_IN_MONTHS[__month_selected] + 1) \
+				if __day == 0 or __day > DAYS_IN_MONTHS[__month_selected] \
+				else __day
+		__yyyymmdd.y = __month_selected + 1
+		return __yyyymmdd
 	var __description: String = description.text.strip_edges().strip_escapes()
-	var __year: int = abs(year.text.strip_edges().strip_escapes().to_int())
-	var __day: int = abs(day.text.strip_edges().strip_escapes().to_int())
-	var __new_yyyymmdd: Vector3i = Vector3i.ZERO
-	__new_yyyymmdd.x = 1999 if __year == 0 else __year
-	__new_yyyymmdd.z = ((__day - 1) % DAYS_IN_MONTHS[month.selected] + 1) \
-			if __day == 0 or __day > DAYS_IN_MONTHS[month.selected] \
-			else __day
-	__new_yyyymmdd.y = month.selected + 1
+	var __new_yyyymmdd: Vector3i = __get_yyyymmdd.call(
+		abs(day.text.strip_edges().strip_escapes().to_int()),
+		abs(year.text.strip_edges().strip_escapes().to_int()),
+		month.selected
+	)
 	return Date.new(__new_yyyymmdd, __description)
 
 func collect() -> Dictionary:
