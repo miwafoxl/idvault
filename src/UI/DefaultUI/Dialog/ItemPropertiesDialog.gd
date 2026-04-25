a1f81ae9fed33370a09214eaba3b0dfc76a6a3a4
Amanda Severo <miwafoxl@proton.me>
2026-04-24 23:34:36 -0300

    ItemProperties: use unique access name for editable_list widget

--- src/UI/DefaultUI/Dialog/ItemPropertiesDialog.gd
+++ src/UI/DefaultUI/Dialog/ItemPropertiesDialog.gd
@@ -1,16 +1,10 @@
 extends DefaultUI_Dialog
 class_name DefaultUI_ItemPropertiesDialog
 
-@export_category("INTERNAL NODES")
-@export var editable_list: DefaultUI_EditableList
-
-@export_category("GENERAL")
-@export var label_item: Label
-
 #region OVERRIDES
 
 func enter_request() -> void:
-	var __new_property: Dictionary = editable_list.collect_item_node_data()
+	var __new_property: Dictionary = %EDITABLELIST.collect_item_node_data()
 	trigger.emit(Trigger.new(
 		Trigger.TriggerTypes.ACTION,
 		&"property.edit.apply", __new_property
@@ -18,7 +12,7 @@ func enter_request() -> void:
 	handle_close_request.emit(alias)
 
 func close_request(__confirm: bool = false) -> void:
-	var __new_property: Dictionary = editable_list.collect_item_node_data()
+	var __new_property: Dictionary = %EDITABLELIST.collect_item_node_data()
 	if not __new_property.is_empty() and not __confirm:
 		trigger.emit(Trigger.new(
 			Trigger.TriggerTypes.UI_REQUEST,
@@ -37,10 +31,10 @@ func _update_arguments() -> void:
 	if __item == null:
 		printerr("ItemPropertiesDialog: item parameter not received")
 	self.set_title(tr(&"DIALOG.ITEM_PROPERTIES.TITLE") + ": %s" % __item.id)
-	editable_list.contents = [DefaultUI_ItemHolder.new(__item)]
-	editable_list.reload_contents()
-	if not editable_list.trigger.is_connected(trigger.emit):
-		editable_list.trigger.connect(trigger.emit)
+	%EDITABLELIST.contents = [DefaultUI_ItemHolder.new(__item)]
+	%EDITABLELIST.reload_contents()
+	if not %EDITABLELIST.trigger.is_connected(trigger.emit):
+		%EDITABLELIST.trigger.connect(trigger.emit)
 
 func _on_button_apply_button_down() -> void:
 	enter_request()
