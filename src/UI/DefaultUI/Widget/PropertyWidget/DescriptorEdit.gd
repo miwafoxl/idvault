b3636661e84fc1e8bc46ee3891932618955846b3
Amanda Severo <miwafoxl@proton.me>
2026-04-27 20:01:54 -0300

    Descriptor: remove brief and meaning (long description) fields

--- src/UI/DefaultUI/Widget/PropertyWidget/DescriptorEdit.gd
+++ src/UI/DefaultUI/Widget/PropertyWidget/DescriptorEdit.gd
@@ -1,18 +1,13 @@
 extends PropertyWidget
 class_name DescriptorEditWidget
 
-@export var alias: LineEdit
-@export var short: LineEdit
-@export var long: TextEdit
-
 func deserialize(__property: Property) -> void:
 	var __prop: Descriptor = __property
 	related_prop_id = __prop.id
-	alias.text = __prop.alias
-	short.text = __prop.short
-	long.text = __prop.long
-	for __control: Control in [alias, short, long]:
-		__control.set_meta(&"unchanged", __control.text)
+	%LINE_ALIAS.set_text(__prop.alias)
+	%LINE_PRIORITY.set_text(str(__prop.priority))
+	for __control: Control in [%LINE_ALIAS, %LINE_PRIORITY]:
+		__control.set_meta(UNCHANGED_META_STR, __control.text)
 
 func collect() -> Dictionary:
 	var __collected: Dictionary = {}
@@ -28,15 +23,14 @@ func collect() -> Dictionary:
 
 func check_if_changed() -> bool:
 	var __changed: int = 0
-	for __control: Control in [alias, short, long]:
+	for __control: Control in [%LINE_ALIAS, %LINE_PRIORITY]:
 		__changed += int(__control.get_meta(UNCHANGED_META_STR) != __control.text)
 	return __changed > 0
 
 func get_as_property() -> Property:
-	var __alias: String = alias.text.to_snake_case()
-	var __short: String = short.text.strip_edges().strip_escapes()
-	var __long: String = long.text.strip_edges()
-	return Descriptor.new(__alias, __short, __long)
+	var __alias: String = %LINE_ALIAS.text.to_snake_case()
+	var __priority: int = %LINE_PRIORITY.text.to_int()
+	return Descriptor.new(__alias, __priority)
 
 func trigger_options() -> void:
 	trigger.emit(Trigger.new(
