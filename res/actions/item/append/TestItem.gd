142d1eb80869160f0ab56faac0cb462a25a5dc4b
Amanda Severo <miwafoxl@proton.me>
2026-04-27 14:55:28 -0300

    Action: update items.append.test_item to always add a link item when a descriptor is available

--- res/actions/item/append/TestItem.gd
+++ res/actions/item/append/TestItem.gd
@@ -4,21 +4,21 @@ extends Object
 func run(__manager: ItemManager, __param: Dictionary) -> bool:
 	var __random_props: Array[Property] = [
 		Display.new(RandomString.new("", 12).value, "first test here".repeat(randi() % 3)),
-		Date.new(Vector3i(1999, 2, 1)),
-		RangedDate.new([Vector3i(1999, 1, 1), Vector3i.ZERO])
+		#Date.new(Vector3i(1999, 2, 1)),
+		#RangedDate.new([Vector3i(1999, 1, 1), Vector3i.ZERO])
 	]
+	var __props: Array[Property] = [__random_props.pick_random()]
 	if __manager.unordered_items.size() > 0 and true:
 		var __descriptors: Dictionary = __manager.item_cache.get("by_descriptor", {})
 		var __descriptors_id: Array
 		if not __descriptors.is_empty(): 
 			__descriptors_id = __descriptors.keys()
-			__random_props.append(Link.new(
+			__props.append(Link.new(
 				__manager.unordered_items.pick_random().id, # FROM ID
 				__descriptors_id.pick_random(), # TO ID
 		))
 	
 	var __items: Array[Item]
-	var __props: Array[Property] = [__random_props.pick_random()]
 	if __manager.unordered_items.size() == 0:
 		__props.append(Descriptor.new(["abc", "def", "ghj"].pick_random()))
 	__items.append(Item.new("", __props))
