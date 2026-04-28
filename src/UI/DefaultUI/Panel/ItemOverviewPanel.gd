b010f85af53f162fec63c23e454c75269f7f1b85
Amanda Severo <miwafoxl@proton.me>
2026-04-27 21:09:23 -0300

    ItemOverviewPanel: fix alt header being visible while empty

--- src/UI/DefaultUI/Panel/ItemOverviewPanel.gd
+++ src/UI/DefaultUI/Panel/ItemOverviewPanel.gd
@@ -18,7 +18,7 @@ func update_view(__selected: int = selected) -> void:
 	if items_ref.is_empty(): return
 	var __item: Item = items_ref[selected]
 	for __control: Control in [%BOX_C_DISPLAY, %BOX_C_DESC_CONTENT,
-								%TXT_BRIEF]:
+								%TXT_BRIEF, %TXT_ALT]:
 		__control.set_visible(false)
 	if __item.properties.is_empty(): return
 	for __prop: Property in __item.properties:
