082f4496fc955cdda67564e15b7ce0547f6d9f2a
Amanda Severo <miwafoxl@proton.me>
2026-05-05 18:08:07 -0300

    License: add license for each source file

--- src/UI/DefaultUI/Widget/PropertyWidget.gd
+++ src/UI/DefaultUI/Widget/PropertyWidget.gd
@@ -1,3 +1,21 @@
+# GNU General Public License, version 2 (GPL-2.0-only) notice
+# ---------------------------------------------------------------
+# PropertyWidget.gd
+# ---------------------------------------------------------------
+# Copyright (C) 2026   Amanda Severo   Contact: miwafoxl@proton.me
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see https://www.gnu.org/licenses/.
+
 @abstract
 extends DefaultUI_Widget
 class_name PropertyWidget
