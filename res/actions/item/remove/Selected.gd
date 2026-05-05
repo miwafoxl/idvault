9e2d9996d519481d436a1629177a561602b7e0aa
Amanda Severo <miwafoxl@proton.me>
2026-05-05 01:09:07 -0300

    Action: fix an issue with items.remove.selected where it would remove every even index of given items

--- res/actions/item/remove/Selected.gd
+++ res/actions/item/remove/Selected.gd
@@ -5,10 +5,6 @@ func run(__mod_item: ItemModule, __param: Dictionary) -> bool:
 	if __mod_item.selected_items.is_empty():
 		push_warning("items.remove.selected: Nothing selected")
 		return false
-	var __rm_indexes: Array[int]
-	for __item: Item in __mod_item.selected_items:
-		var __idx: int = __mod_item.unordered_items.find(__item)
-		__rm_indexes.append(__idx)
-	__mod_item.remove_items_unordered(__mod_item.selected_items)
+	__mod_item.remove_items_unordered(__mod_item.selected_items.duplicate())
 	__mod_item.selection_updated.emit()
 	return true
